70fe87be9f7c293e98724cb432a0196a3bbab2c5
Mauricio Chavez <mau_chavez003@hotmail.com>
2024-04-16 20:22:27 -0700

    Update

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.get('/')
 def index():
+    return "Hello World"
+
+@app.get('/aboutme')
+def aboutMe():
     me = {
         "first_name": "Mauricio",
         "last_name": "Chavez" ,
